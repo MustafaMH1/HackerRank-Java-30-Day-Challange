73028317adb1a3d6780713d8553f9e687566bcce
Mustafa <mustafa.mabdelkader91@gmail.com>
2024-04-21 01:53:08 +0200

    Day 0

---
+++ Day 0 - Hello World/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.Scanner;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        // Create a Scanner object to read input from stdin.
+    Scanner scan = new Scanner(System.in);
+    String inputString = scan.nextLine();
+    scan.close();
+
+        // Print a string literal saying "Hello, World." to stdout.
+        System.out.println("Hello, World.");
+
+    }
+}
